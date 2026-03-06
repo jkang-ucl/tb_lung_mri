19bbfb6de506680ff8ee2e5208c0fd5fb885c09e
jkang-ucl <jungwoo.kang@ucl.ac.uk>
2026-03-06 17:44:02 +0000

    Delete config/local_config.m

--- config/local_config.m
+++
@@ -1,7 +0,0 @@
-function cfg = local_config()
-
-    % Path definitions
-    cfg.paths.rawData = '/Users/jungwookang/Documents/Lung MRI DICOM DATA.nosync/RawDICOM';
-    cfg.paths.processedData = '/Users/jungwookang/Documents/Lung MRI DICOM DATA.nosync/Processed';
-
-end
